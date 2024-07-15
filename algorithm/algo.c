126a409d0f86a15e9ce5c99e4aaa894bcaa67f21
Youssef Regragui <yregragu@c4r10p5.1337.ma>
2024-07-15 23:27:25 +0100

    indexing

--- algorithm/algo.c
+++ algorithm/algo.c
@@ -12,7 +12,7 @@
 
 #include "../includes/push_swap.h"
 
-// int	ft_range(t_list *b)
+// int	ft_range(t_list *a)
 // {
 // 	int size;
 
@@ -26,101 +26,55 @@
 // }
 
 
-static void	indexing(t_list **a, int *array, int size)
+static void	indexing(t_list **a)
 {
-	t_list	*ls;
-	int 	i;
-	
-	ls = *a;
-	while(ls)
+	t_list	*tmp;
+	t_list	*tmp1;
+
+	tmp = *a;
+	tmp1 = *a;
+	while(tmp)
 	{
-		i = 0;
-		while(i < size)
+		tmp->index = 0;
+		tmp = tmp->next;
+	}
+	tmp = *a;
+	while(tmp)
+	{
+		while(tmp1)
 		{
-			if(ls->content == array[i])
-			{
-				printf("%i||", array[i]);
-				ls->index = i;
-				break;
-			}
-			i++;
+			if(tmp->content > tmp1->content)
+				tmp->index++;
+			tmp1 = tmp1->next;
 		}
-		ls = ls->next;
+		tmp1 = *a;
+		tmp = tmp->next;
 	}
 }
-static void	bubble_sort(int *array, int size)
+static	void algo(t_list **a, t_list **b)
 {
-	int	i;
-	int	j;
-	int	temp;
-
-	i = 0;
-	while(i < size - 1)
-	{
-		j = i + 1;
-		while(j < size - i - 1)
-		{
-			if(array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-			}
-			j++;
-		}
-		i++;
-	}
-}
-
-static	void sort_array(t_list **a)
-{
-	int		*array;
-	int		i;
-	int		size;
-	t_list	*temp;
-
-	size = lst_size(*a);
-	i = 0;
-	temp = *a;
-	array = malloc(sizeof(int) * size);
-	if(!array)
-		return;
-	while (temp)
-	{
-		array[i] = temp->content;
-		i++;
-		temp = temp->next;
-	}
-	bubble_sort(array, size);
-	indexing(a, array, size);
-	free(array);
-}
-
-
-// static	void algo(t_list **a, t_list **b)
-// {
-// 	int range = 15;
-// 	int size;
+	// int range = 15;
+	int size;
 	
 
-//     // range = ft_range(size);
-// 	while (*a)
-//     {
-// 		size = lst_size(*b);
-//         if ((*a)->index < size)
-//         {
-//         	push_b(a, b);
-//     		rotate_b(b, 1);
-// 		}
-// 		else if ((*a)->index <= size + range)
-// 			push_b(a, b);
-// 		else
-// 			rotate_a(a, 1);
-// 	}
-// }
+    // range = ft_range(a);
+	while (*a)
+    {
+		size = lst_size(*b);
+        if ((*a)->index < size)
+        {
+        	push_b(a, b);
+    		rotate_b(b, 1);
+		}
+		else if ((*a)->index <= size + range)
+			push_b(a, b);
+		else
+			rotate_a(a, 1);
+	}
+}
 
 void    push_swap(t_list **a, t_list **b)
 {
-    sort_array(a);
-	// algo(a, b);
+    indexing(a);
+	algo(a, b);
 }
